b8f4b94b2870c83b1c91793fa348b98908a152db
shahadathhs <shahadathhossensajib732@gmail.com>
2024-11-01 18:54:02 +0600

    feat: video on type assertion completed

--- src/typeAssertion.ts
+++ src/typeAssertion.ts
@@ -1,3 +1,21 @@
 {
-  
+    // Type Assertion
+    let anything: any;
+    anything = 'Next Level';
+
+    (anything as string)
+
+    const kgToGm = (value: string | number | undefined): string | number | undefined => {
+        if (typeof value === 'string') {
+            return parseFloat(value) * 1000;
+        } else if (typeof value === 'number') {
+            return value * 1000;
+        } else {
+            return "Not convertable value!";
+        }
+    }
+
+    const resultOne = kgToGm(20) as number;
+    const resultTwo = kgToGm("20") as string;
+    const resultThree = kgToGm(undefined) as undefined;
 }
